1bea5e50301e3948a12bf16b10b59fcacc151ba2
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-09-21 17:13:48 +0900

    quick start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>My Third Angular 2 App</h1>',
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
